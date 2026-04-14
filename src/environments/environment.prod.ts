be987f4ff0e1aef51cd9297f0c11735236f0e5c1
Tim <waiting0201@gmail.com>
2026-04-14 17:09:12 +0800

    line login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://jabez-api.azurewebsites.net/api',
-  lineLoginChannelId: '',
-  lineCallbackUrl: '',
+  lineLoginChannelId: '2009730684',
+  lineCallbackUrl: 'https://jabez.4webdemo.com/line/bind-callback',
 };
