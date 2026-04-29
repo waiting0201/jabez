30675a10d8719f8aa7610f4402d53874cfabcd95
Tim <waiting0201@gmail.com>
2026-04-29 11:56:13 +0800

    -

---
+++ Api/Data/Migrations/20260429035353_UpdateTravelPaymentStep1ToDesignated.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jabez.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTravelPaymentStep1ToDesignated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ApprovalSteps",
+                keyColumn: "Id",
+                keyValue: 50,
+                columns: new[] { "JobTitleId", "Note", "UseApplicantDepartment", "UseApplicantDesignated" },
+                values: new object[] { null, "指定初核", false, true });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ApprovalSteps",
+                keyColumn: "Id",
+                keyValue: 50,
+                columns: new[] { "JobTitleId", "Note", "UseApplicantDepartment", "UseApplicantDesignated" },
+                values: new object[] { 4, "部門主管初核", true, false });
+        }
+    }
+}
