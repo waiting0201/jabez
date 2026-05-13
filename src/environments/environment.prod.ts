5f0c7cc3c7ba53236a9acb149fd3b247489e531c
Tim <waiting0201@gmail.com>
2026-05-13 13:01:04 +0800

    line callback url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://jabez-api.azurewebsites.net/api',
   lineLoginChannelId: '2009730684',
-  lineCallbackUrl: 'https://jabez.4webdemo.com/line/bind-callback',
+  lineCallbackUrl: 'https://jacore.jacreative.com.tw/line/bind-callback',
   // LINE 官方帳號加好友 URL（格式 https://line.me/R/ti/p/@{basicId}）— 用於已綁定但未加 OA 好友的用戶
   lineOaFriendUrl: 'https://line.me/R/ti/p/@483emxpy'
 };
